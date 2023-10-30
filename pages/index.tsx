36c9d9da5f66180854297c49569aef14931f728e
ludmilpaulo <ludmilpaulo@gmail.com>
2023-10-30 15:12:00 +0200

    fb

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,33 +23,9 @@ const IndexPage = () => {
       <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {/* Facebook Messenger Chat Plugin */}
-        <div id="fb-root"></div>
-        <div id="fb-customer-chat" className="fb-customerchat"></div>
+       
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var chatbox = document.getElementById('fb-customer-chat');
-              chatbox.setAttribute("page_id", "600949946906137");
-              chatbox.setAttribute("attribution", "biz_inbox");
-              window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml: true,
-                  version: 'v18.0',
-                });
-              };
-              (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s);
-                js.id = id;
-                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                fjs.parentNode.insertBefore(js, fjs);
-              })(document, 'script', 'facebook-jssdk');
-            `,
-          }}
-        />
+      
       </Head>
       <ErrorBoundary>
 
