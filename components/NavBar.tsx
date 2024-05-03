e1cb75ed3addbd45108319f70da4cee31876f879
Nigel <hypej.10@gmail.com>
2024-05-03 18:01:23 +0200

    fix nav on mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -34,53 +34,90 @@ const NavBar = () => {
 
   return (
     <nav className="relative flex items-center justify-between max-w-full px-6 py-4">
-    <div className="flex items-center justify-start">
-      <div className="mr-auto logo">
-        <Link href="/">
-          <Image
-            src={logoLandscape}
-            alt={dataObj?.title}
-            width={400}
-            height={83}
-            className="h-[83px]"
-          />
-        </Link>
-      </div>
-    </div>
-    <div className="items-center flex-1">
-      <div className="flex justify-center">
-        <div className="flex space-x-8">
-          <Link className="flex items-center text-[#e20613] text-lg font-black" href={"/AboutPage"}>
-            <span>About Us</span>
-          </Link>
-  
-          <Link
-            className="flex items-center text-[#e20613] text-lg font-black"
-            href={"/ServicePage"}
-          >
-            <span>Services</span>
-          </Link>
-  
-          <Link
-            className="flex items-center text-[#e20613] text-lg font-black"
-            href={"/Contactpage"}
-          >
-            <span>Contact Us</span>
+      <div className="flex items-center justify-start">
+        <div className="mr-auto logo">
+          <Link href="/">
+            <Image
+              src={logoLandscape}
+              alt={dataObj?.title}
+              // width={400}
+              // height={83}
+              className="h-[70px] w-[180px] cursor-pointer"
+            />
           </Link>
         </div>
       </div>
-    </div>
-    <div className="flex items-center justify-end">
-      <div className="flex space-x-4">
-        {/* Example social media icons */}
-        <a href="https://www.facebook.com/thembalethusa.co.za" className="text-[#1a3f85] text-lg"><i className="fab fa-facebook"></i></a>
-        <a href="#" className="text-[#1a3f85] text-lg"><i className="fab fa-twitter"></i></a>
-        <a href="https://www.facebook.com/thembalethusa.co.za" className="text-[#1a3f85] text-lg"><i className="fab fa-instagram"></i></a>
-      </div>
-    </div>
-  </nav>
-  
+      <div className="items-center flex-1">
+        <div className="flex justify-end mr-10">
+          {isMobileMenuOpen ? (
+            <IoClose
+              className="block md:hidden text-2xl "
+              onClick={toggleMobileMenu}
+            />
+          ) : (
+            <IoMenu
+              className="block md:hidden text-2xl mt-[-11px] mr-[-20px]"
+              onClick={toggleMobileMenu}
+            />
+          )}
+          <div
+            className={`md:flex md:justify-between lg:w-[40%] md:w-[75%] ${
+              isMobileMenuOpen
+                ? "block fixed right-0 top-0 bottom-0 bg-white px-12 pt-5"
+                : "hidden"
+            }`}
+          >
+            {isMobileMenuOpen && (
+              <IoClose
+                color="#e20613"
+                className="block md:hidden text-2xl text-red-500 ml-[-30px]"
+                onClick={toggleMobileMenu}
+              />
+            )}
+            <Link
+              className="flex items-center text-[#e20613] text-lg font-black"
+              href={"/AboutPage"}
+            >
+              <span>About Us</span>
+            </Link>
 
+            <Link
+              className="flex items-center text-[#e20613] text-lg font-black"
+              href={"/ServicePage"}
+            >
+              <span>Services</span>
+            </Link>
+
+            <Link
+              className="flex items-center text-[#e20613] text-lg font-black"
+              href={"/Contactpage"}
+            >
+              <span>Contact Us</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-end">
+        <div className="flex space-x-4">
+          {/* Example social media icons */}
+          <a
+            href="https://www.facebook.com/thembalethusa.co.za"
+            className="text-[#1a3f85] text-lg"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="#" className="text-[#1a3f85] text-lg">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/thembalethusa.co.za"
+            className="text-[#1a3f85] text-lg"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 };
 
